b44bcc730b2b5560984d219b5a2c5ab477223868
박재복 <pjb24@wizbrain.co.kr>
2022-12-12 23:41:24 +0900

    gitignore Visual Studio 형식으로 변경 IPC 프로젝트 추가

---
+++ InterProcessCommunication/InterProcessCommunication/NamedPipe.cpp
@@ -0,0 +1,2 @@
+#include "pch.h"
+#include "NamedPipe.h"
---
+++ InterProcessCommunication/InterProcessCommunication/Socket.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef DLLEXPORT
+#define DllExport __declspec(dllexport)
+#else
+#define DllExport __declspec(dllimport)
+#endif // DLLEXPORT
+
+class DllExport Socket
+{
+};
+
